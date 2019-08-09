ec5092deb77d1464359507d8812b05ff9163bb26
maciej12203 <48186301+maciej12203@users.noreply.github.com>
2019-08-09 13:55:19 +0200

    Update linux-apt-package.sh

--- Artifacts/linux-postgres/linux-apt-package.sh
+++ Artifacts/linux-postgres/linux-apt-package.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-sudo rpm -ivh https://yum.postgresql.org/9.6/redhat/rhel-7.3-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+mkdir –p /var/databases/pgsql-9.6/data
+sudo yum -y localinstall https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo yum -y update
 sudo yum install -y postgresql96 postgresql96-server postgresql96-libs postgresql96-contrib postgresql96-devel
-sudo sed -i -e"s/^#listen_addresses = 'localhost'.*$/listen_addresses = '*'/" /usr/pgsql-9.6/share/postgresql.conf.sample
-sudo sed -i -e"s/^#port = 5432.*$/port = 5432/" /usr/pgsql-9.6/share/postgresql.conf.sample
-sudo /usr/pgsql-9.6/bin/postgresql96-setup initdb
+sudo chown –R postgres:postgres /var/databases/pgsql-9.6
+su – postgres –c ‘/usr/pgsql-9.6/bin/initdb –D /var/databases/pgsql-9.6/data’
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/private/localhost.key -out /etc/ssl/certs/localhost.crt
+cp /etc/pki/tls/certs/localhost.crt /var/databases/pgsql-9.6/data/server.crt
+cp /etc/pki/tls/private/localhost.key /var/databases/pgsql-9.6/data/server.key
+echo –e “.include /usr/lib/systemd/system/postgresql-96.service\n\n[Service]\nEnvironment=PGDATA=/var/databases/pgsql-9.6/data/\n” > /etc/systemd/system/postgresql-9.6.service
+sed -i -e"s/^#listen_addresses = 'localhost'.*$/listen_addresses = '*'/" /var/databases/pgsql-9.6/data/postgresql.conf
+sed -i -e"s/^ssl = off.*$/ssl = on/" /var/databases/pgsql-9.6/data/postgresql.conf
+
 sudo systemctl enable postgresql-9.6
 sudo systemctl start postgresql-9.6
