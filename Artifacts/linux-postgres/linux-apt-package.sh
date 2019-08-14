b5feea7ed15b02c73d78136557061e17d67d0bf9
maciej12203 <48186301+maciej12203@users.noreply.github.com>
2019-08-14 09:26:10 +0200

    Update linux-apt-package.sh

--- Artifacts/linux-postgres/linux-apt-package.sh
+++ Artifacts/linux-postgres/linux-apt-package.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-mkdir –p /var/databases/pgsql-9.6/data
-yum install  https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-redhat96-9.6-3.noarch.rpm -y
-sudo yum -y install postgresql96 postgresql96-server postgresql96-libs postgresql96-contrib
-sudo chown –R postgres:postgres /var/databases/pgsql-9.6
-su – postgres –c ‘/usr/pgsql-9.6/bin/initdb –D /var/databases/pgsql-9.6/data’
+mkdir –p /var/databases/pgsql-9.2/data
+yum install postgresql-server -y
+yum install postgresql-contrib -y
+sudo chown –R postgres:postgres /var/databases/pgsql-9.2
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/private/localhost.key -out /etc/ssl/certs/localhost.crt
-cp /etc/pki/tls/certs/localhost.crt /var/databases/pgsql-9.6/data/server.crt
-cp /etc/pki/tls/private/localhost.key /var/databases/pgsql-9.6/data/server.key
-echo –e “.include /usr/lib/systemd/system/postgresql-9.6.service\n\n[Service]\nEnvironment=PGDATA=/var/databases/pgsql-9.6/data/\n” > /etc/systemd/system/postgresql-9.6.service
-sed -i -e"s/^#listen_addresses = 'localhost'.*$/listen_addresses = '*'/" /var/databases/pgsql-9.6/data/postgresql.conf
-sed -i -e"s/^ssl = off.*$/ssl = on/" /var/databases/pgsql-9.6/data/postgresql.conf
+cp /etc/pki/tls/certs/localhost.crt /var/databases/pgsql-9.2/data/server.crt
+cp /etc/pki/tls/private/localhost.key /var/databases/pgsql-9.2/data/server.key
+echo –e “.include /usr/lib/systemd/system/postgresql.service\n\n[Service]\nEnvironment=PGDATA=/var/databases/pgsql-9.2/data/\n” > /etc/systemd/system/postgresql.service
+sed -i -e"s/^#listen_addresses = 'localhost'.*$/listen_addresses = '*'/" /var/databases/pgsql-9.2/data/postgresql.conf
+sed -i -e"s/^#ssl = off.*$/ssl = on/" /var/databases/pgsql-9.2/data/postgresql.conf
+su – postgres –c ‘initdb –D /var/databases/pgsql-9.2/data’
 
-sudo systemctl enable postgresql-9.6
-sudo systemctl start postgresql-9.6
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
